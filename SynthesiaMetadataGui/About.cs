21a5eeaf899a96e0df747f29c86ed8e5cbf17098
npiegdon@gmail.com <npiegdon@gmail.com@f3b0126c-c088-11dd-94e2-cbf2318f60c0>
2013-07-30 00:04:45 +0000

    Hand-updating the version number each build is silly.

--- SynthesiaMetadataGui/About.cs
+++ SynthesiaMetadataGui/About.cs
@@ -15,7 +15,6 @@ namespace Synthesia
             InitializeComponent();
             this.Text = String.Format("About {0}", AssemblyTitle);
             this.labelProductName.Text = AssemblyProduct;
-            this.labelVersion.Text = String.Format("Version {0}", AssemblyVersion);
             this.labelCopyright.Text = AssemblyCopyright;
             this.labelCompanyName.Text = AssemblyCompany;
             this.textBoxDescription.Text = AssemblyDescription;
