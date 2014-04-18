a38ab3a913a29965b6a262704d50faa7760e4796
Nicholas Piegdon <nicholas@synthesiagame.com>
2014-04-18 14:32:10 -0400

    Initial (still empty) MonoMac GUI

---
+++ mac-gui/AppDelegate.designer.cs
@@ -0,0 +1,10 @@
+﻿
+namespace macgui
+{
+   // Should subclass MonoMac.AppKit.NSResponder
+   [MonoMac.Foundation.Register ("AppDelegate")]
+   public partial class AppDelegate
+   {
+   }
+}
+
---
+++ mac-gui/MainWindow.designer.cs
@@ -0,0 +1,15 @@
+﻿
+namespace macgui
+{
+   // Should subclass MonoMac.AppKit.NSWindow
+   [MonoMac.Foundation.Register ("MainWindow")]
+   public partial class MainWindow
+   {
+   }
+   // Should subclass MonoMac.AppKit.NSWindowController
+   [MonoMac.Foundation.Register ("MainWindowController")]
+   public partial class MainWindowController
+   {
+   }
+}
+
