ab26681fdf72ff42df5509f1069eaf5b8a90ae0a
Nicholas Piegdon <nicholas@synthesiagame.com>
2017-06-20 16:11:15 -0400

    A little project cleanup; bump .NET to 4.5

--- win-gui/GroupEditor.cs
+++ win-gui/GroupEditor.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace Synthesia
--- win-gui/MetadataEditor.cs
+++ win-gui/MetadataEditor.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
 using System.IO;
-using System.Xml.Linq;
+using System.Linq;
 using System.Reflection;
+using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace Synthesia
 {
